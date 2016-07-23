f2ac5145a072a1f46b0d3bb1531299a3f06bc95e
Perevozchykov Kostiantyn <skvoz.ne@gmail.com>
2016-07-23 15:59:29 +0300

    fix

--- config/db.php
+++ config/db.php
@@ -1,8 +1,13 @@
 <?php
 
-$_fn=realpath(__DIR__."/..")."/sqlitedb.sqlite";
+//$_fn=realpath(__DIR__."/..")."/sqlitedb.sqlite";
 
 return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'sqlite:'.$_fn,
     'class' => 'yii\db\Connection',
-    'dsn' => 'sqlite:'.$_fn,
+    'dsn' => 'pgsql:host=localhost;dbname=hobby-dev',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
 ];
---
+++ migrations/m160723_125414_tbl_main.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+class m160723_125414_tbl_main extends Migration
+{
+    public function up()
+    {
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        /** @var Migration $this */
+        $this->createTable('main', [
+           'id' => $this->primaryKey(),
+            'date' => $this->varchar(255),
+            'owner' => $this->varchar(255),
+            'guest' => $this->varchar(255),
+            'stadium' => $this->integer(11),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        echo "m160723_125414_tbl_main cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
