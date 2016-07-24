a0542643287be46557a6780ea462f9ed5de3eab7
Perevozchykov Kostiantyn <skvoz.ne@gmail.com>
2016-07-24 21:33:48 +0300

    fix db config

--- config/db.php
+++ config/db.php
@@ -6,8 +6,8 @@ return [
 //    'class' => 'yii\db\Connection',
 //    'dsn' => 'sqlite:'.$_fn,
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=hobby-dev',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=ec2-54-243-200-63.compute-1.amazonaws.com;dbname=d1eoqtg8bdgafl',
+    'username' => 'olicfdnxffficg',
+    'password' => 'kQXyD1Va3AYqTX_MXkGOZnecyD',
     'charset' => 'utf8',
 ];
